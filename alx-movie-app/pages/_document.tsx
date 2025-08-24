d91347b841e0d0757f6a15217904d8f2457dfd40
Uyearit <uyearit23@gmail.com>
2025-08-24 17:52:07 +0100

    feat: link manifest.json and set theme color in _document.tsx.

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
